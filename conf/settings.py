c0b176752133f0ee17a648166e6a6684f95aca08
shaxriyorsharofov <shaxriyorsharofov911@gmail.com>
2025-09-13 13:17:20 +0500

    host update

--- conf/settings.py
+++ conf/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["shaxriyorpy.uz", "www.shaxriyorpy.uz", "137.184.15.220"]
 
 
 # Application definition
